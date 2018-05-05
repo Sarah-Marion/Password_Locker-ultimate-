c3467a4878e2d721542aae75395f003beb8427c7
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-05 16:43:49 +0300

    created a test tocheck if a user exists

--- user_data_test.py
+++ user_data_test.py
@@ -1,7 +1,6 @@
 import unittest
 from user_data import User
 
-
 class user_test(unittest.TestCase):
     """
     Test class that defines test cases for the user_data class behaviours
@@ -9,37 +8,47 @@ class user_test(unittest.TestCase):
         unittest.Testcase: TestCase class that helps in creating test cases
     """
 
-def setUp(self):
-    """
-    set up method to run each test case
-    """
-    self.new_user = User("username","password")
+    def setUp(self):
+        """
+        set up method to run each test case
+        """
+        self.new_user = User("username","password")
 
 
-def test_init(self):
-    """
-    test init test case to test if the object is initialized properly
-    """
+    def test_init(self):
+        """
+        test init test case to test if the object is initialized properly
+        """
 
-    self.assertEqual(self.new_user.username, "username")
-    self.assertEqual(self.new_user.password, "password")
-
-
-def test_create_new_account(self):
-    """
-    test_create_new_account test case to test if the new user object is
-    saved into the user list
-    """
-
-    self.new_user.save_user()
-    self.assertEqual(len(User.user_list), 1)
+        self.assertEqual(self.new_user.username, "username")
+        self.assertEqual(self.new_user.password, "password")
 
 
-def tearDown(self):
-    """
-    teardown method that does clean up after each test case has run
-    """
-    User.user_list = []
+    def test_create_new_account(self):
+        """
+        test_create_new_account test case to test if the new user object is
+        saved into the user list
+        """
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+
+    def tearDown(self):
+        """
+        teardown method that does clean up after each test case has run
+        """
+        User.user_list = []
+
+
+    def test_check_user_exist(self):
+        """
+        test_check_user_exist to test if a user exists or not
+        """
+        self.test_user = User("username", "password1")
+        self.test_user.save_user()
+        found_user = User.find("username")
+        self.assertTrue(found_user)
 
 
 
