b1167ff96aed4cdfa7f2ce6f6cef794c0da47091
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-05 10:29:42 +0300

    created a class that generartes new instances of users

---
+++ user_data.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    class that generates new instances of users
+    """
+    def __init__(self, username, password):
+        
+        self.username = username
+        self.password = password
+    user_list = []
+
+    
+    
