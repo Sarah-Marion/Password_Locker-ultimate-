eac700622c5d53d15c14e007d1334a7a7d285dd0
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-05 11:54:43 +0300

    created a class that generates new instances of user credentials

---
+++ credential.py
@@ -0,0 +1,15 @@
+import pyperclip
+import string
+import random
+
+class Credential:
+    """
+    class that generates new instances of user credentials
+    """
+    def __init__(self, profile_name, profile_username = None, profile_email = None, profile_password = None):
+        self.profile_name = profile_name
+        self.profile_username = profile_username
+        self.profile_email = profile_email
+        self.profile_password = profile_password
+    
+    profile_list = []
