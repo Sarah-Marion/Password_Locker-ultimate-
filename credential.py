a1e4155695038afc4340c0af1babcfcc661c5c80
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-05 13:57:46 +0300

    created a class method to search for profiles

--- credential.py
+++ credential.py
@@ -24,7 +24,7 @@ class Credential:
 
 
     @classmethod
-    def check_profile_exist(cls,profile_name, profile_username = None, profile_email = None):
+    def check_profile_exist(cls, profile_name, profile_username = None, profile_email = None):
         """
         check_profile_exist method checks if there is another matching or similar profile
         Args:
@@ -34,8 +34,23 @@ class Credential:
         """
         
         for profile in cls.profile_list:
-            if(profile.profile_name == profile_name) or (profile.profile_username == profile_username) or (profile.profile_email == profile_email)
+            if (profile.profile_name == profile_name) or (profile.profile_username == profile_username) or (profile.profile_email == profile_email):
                 return True
             else:
-                return False    
+                return False 
+
+
+
+    @classmethod
+    def search_profile(cls, param):
+        """
+        search_profile method that searches for profile/s based on profile name, username or profile_email
+        Args:
+            profile to search if it exists
+        """
+
+        for profile in cls.profile_list:
+            while (profile.profile_name == param) or (profile.profile_username == param) or (profile.profile_email == param):
+                return profile
+
 
