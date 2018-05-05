e0dde942bb8c70473e0f7c0a314c7ed2e7057f6d
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-05 10:36:12 +0300

    created a method to save user object into the user list

--- user_data.py
+++ user_data.py
@@ -6,7 +6,14 @@ class User:
         
         self.username = username
         self.password = password
+    
     user_list = []
 
-    
+
+    def save_user(self):
+        """
+        save_user method saves user object into user_list
+        """
+        User.user_list.append(self)
+
     
